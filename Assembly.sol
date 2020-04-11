2ee9cd964ef360a75c51897950e14c05efeb0dc6
Marc Wäckerlin <marc@mrw.dev>
2020-04-11 17:32:47 +0200

    Completely redesigned interface, use lib, check MRWTest for reference

--- Assembly.sol
+++ Assembly.sol
@@ -14,13 +14,58 @@ contract Assembly is owned {
         api = _api;
     }
 
+    function numSecrets() public view returns (uint256) {
+        return secrets.length;
+    }
+
+    function numVotings() public view returns (uint256) {
+        return votings.length;
+    }
+    
+ /*    // Size of a word, in bytes.
+    uint internal constant WORD_SIZE = 32;
+
+    function uint2bytes(uint _i) internal pure returns (bytes memory) {
+        if (_i == 0) {
+            return bytes("0");
+        }
+        uint j = _i;
+        uint len;
+        while (j != 0) {
+            len++;
+            j /= 10;
+        }
+        bytes memory bstr = new bytes(len);
+        uint k = len - 1;
+        while (_i != 0) {
+            bstr[k--] = byte(uint8(48 + _i % 10));
+            _i /= 10;
+        }
+        return bstr;
+    }
+
+    function concat(bytes memory a, bytes memory b) internal pure returns (bytes memory) {
+        return abi.encodePacked(a, b);
+    } */
+
+/*     function verify(string memory secret, uint8 v, bytes32 r, bytes32 s) public pure 
+    returns (bytes32 hash, address sender, bytes memory package, bytes memory bsecret, bytes memory length, bytes memory prefix) {
+        bsecret = bytes(secret);
+        length = uint2bytes(bsecret.length);
+        prefix = bytes("\x19Ethereum Signed Message:\n");
+        package = abi.encodePacked(abi.encodePacked(prefix, length), bsecret);
+        hash = sha3(prefix);
+        sender = ecrecover(hash, v, r, s);
+    } */
+
+    function verify(string memory secret, uint8 v, bytes32 r, bytes32 s) public pure returns (address sender) {
+        bytes32 hash = keccak256(bytes(secret));
+        sender = ecrecover(hash, v, r, s);
+    }
+
     function register(string memory secret, uint8 v, bytes32 r, bytes32 s) public {
         require(msg.sender==api, "only the API is allowed to register");
-        // reg is a signed message
-        // extract signer's address and the shared secret
-        // set registered and index
-        bytes32 h = sha256(bytes(secret));
-        address shareholder = ecrecover(h, v, r, s);
+        address shareholder = verify(secret, v, r, s);
         require(shareholder!=address(0x0), "identification failed due to invalid signature");
         require(registrations[secret]==address(0x0), "secret has already been used");
         require(bytes(shareholders[shareholder]).length==0, "you are already registered");
